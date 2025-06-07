87c540197807f5e1e7f61a9cff49f8e7aa268c0b
therohit15 <rohitk15102004@gmail.com>
2025-06-07 04:00:19 -0400

    module 3.2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,8 +30,13 @@ DEBUG = True
 
 
 ALLOWED_HOSTS = ['localhost',
-'https://rohitk151020-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://rohitk151020-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+'https://rohitk151020-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+'https://*.proxy.cognitiveclass.ai',
+'https://rohitk151020-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = ['https://rohitk151020-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://*.proxy.cognitiveclass.ai',
+    'https://rohitk151020-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/'
+]
 
 
 REST_FRAMEWORK = {
