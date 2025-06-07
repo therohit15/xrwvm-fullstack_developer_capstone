0b51a63d4838128e34ff34121bbe20550fe69bea
Rohit <132490663+therohit15@users.noreply.github.com>
2025-06-07 17:54:23 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -31,7 +31,7 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 class CarModel(models.Model):
     # Many-to-One relationship
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
     CAR_TYPES = [
