b80097f6c3ce0fda7e9cc54c0c42e43a7e250cc8
Rohit <132490663+therohit15@users.noreply.github.com>
2025-06-07 17:23:40 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
