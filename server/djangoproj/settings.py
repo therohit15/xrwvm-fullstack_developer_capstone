75b9e88b4c088541f57552a365b91496b0bedb59
therohit15 <rohitk15102004@gmail.com>
2025-06-07 09:16:38 -0400

    module 5

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -36,6 +36,9 @@ ALLOWED_HOSTS = [
     'https://*.proxy.cognitiveclass.ai',
     'https://rohitk151020-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01'
     '.proxy.cognitiveclass.ai/',
+    'https://rohitk151020-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/',
+    'localhost',
+    '127.0.0.1',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
